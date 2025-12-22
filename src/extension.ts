a32f5018f0aa5a33fce661acc70abbed84be0b83
Dz <MathBeWithCode@gmail.com>
2025-12-22 17:13:26 +0800

    single

--- src/extension.ts
+++ src/extension.ts
@@ -5,18 +5,14 @@ import { a, cms, s } from './template';
 export function activate(context: vscode.ExtensionContext) {
 	const folderContextDisposable = vscode.commands.registerCommand('cms-generator.folderContextAction', async (uri: vscode.Uri) => {
 		
-		const fileA = vscode.Uri.joinPath(uri, 'a.dart');
-		const fileS = vscode.Uri.joinPath(uri, 's.dart');
 		const fileCms = vscode.Uri.joinPath(uri, 'cms.dart');
 
-		if ((await Promise.all([fileExists(fileA), fileExists(fileS), fileExists(fileCms)])).some(exists => exists)) {
+		if ((await Promise.all([fileExists(fileCms)])).some(exists => exists)) {
 			vscode.window.showWarningMessage(`One or more files already exist`);
 			return;
 		}
 
 		await Promise.all([
-			writeFile(fileA, Buffer.from(a)),
-			writeFile(fileS, Buffer.from(s)),
 			writeFile(fileCms, Buffer.from(cms))
 		]);
 
--- src/template.ts
+++ src/template.ts
@@ -22,12 +22,25 @@ sealed class S with _$S {
 
 const cms = `
 import 'package:cms/cms.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-import 's.dart';
-import 'a.dart';
+part 'cms.freezed.dart';
 
-export 's.dart';
-export 'a.dart';
+@Freezed(
+  map: .new(map: false, mapOrNull: false, maybeMap: false),
+  when: .new(when: false, whenOrNull: false, maybeWhen: false),
+)
+sealed class A with _$A {
+  const factory A.init() = Init;
+}
+
+@Freezed(
+  map: .new(map: false, mapOrNull: false, maybeMap: false),
+  when: .new(when: false, whenOrNull: false, maybeWhen: false),
+)
+sealed class S with _$S {
+  const factory S.zero() = Zero;
+}
 
 class M extends Cms<S, A> {
   M() : super(const Zero()) ;
